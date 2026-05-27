6c15ca1b5ce2b574188a3e15354ac383a7c29ca1
Cassandra GM <cassie.gimebi@gmail.com>
2026-05-27 19:17:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Pokémon Boxes Data
 const defaultInventory = [
+  { id: '140', name: 'アビスアイ', count: 0, countUnsealed: 0, image: 'https://placehold.co/150x150/fdfdfd/333333?text=Box' },
   { id: '1', name: 'ニンジャスピナー', count: 0, countUnsealed: 0, image: 'https://www.pokemon-card.com/products/2026/images/m4pkg.jpg' },
   { id: '2', name: 'ムニキスロゼロ', count: 0, countUnsealed: 0, image: 'https://www.pokemon-card.com/products/2025/images/m3.jpg' },
   { id: '4', name: 'MEGAドリーム', count: 0, countUnsealed: 0, image: 'https://www.pokemon-card.com/products/2025/images/m2a.jpg' },
